f5f7a81ae540f8bc4eb4ab07271471062fcc9ba4
Fanouu <mileniumshad@gmail.com>
2023-10-08 17:21:32 -0400

    .

--- src/voicechat/packets/ClientRequestConnection.py
+++ src/voicechat/packets/ClientRequestConnection.py
@@ -4,3 +4,8 @@ from src.voicechat.packets.PacketId import PacketId
 
 class ClientRequestConnection(Packet):
     packet_id = PacketId.CLIENT_REQUEST_CONNECTION
+
+    magic: bytes = None
+
+    def decodePayload(self):
+        self.magic = self.read_magic()
--- src/voicechat/packets/UnconnectedPing.py
+++ src/voicechat/packets/UnconnectedPing.py
@@ -1,4 +1,29 @@
 from src.voicechat.packets.Packet import Packet
 from src.voicechat.packets.PacketId import PacketId
+
+
 class UnconnectedPing(Packet):
     packet_id = PacketId.UNCONNECTED_PING
+
+    client_timestamp: int = None
+    magic: bytes = None
+
+    def read_long(self):
+        if len(self.data) < 8:
+            raise ValueError("Insufficient data to read a long value")
+
+        long_value = int.from_bytes(self.data[:8], byteorder='big', signed=False)
+        self.data = self.data[8:]
+
+        return long_value
+
+    def decodePayload(self):
+        self.client_timestamp = self.read_long()
+        self.magic = self.read_magic()
+
+    def toDict(self) -> dict:
+        dict = super().toDict()
+        dict["magic"] = self.magic
+        dict["client_timestamp"] = self.client_timestamp
+
+        return dict
