630a4e85496166c9df0a5b5f27988405fa584a4b
Fanouu <mileniumshad@gmail.com>
2023-10-11 18:40:54 -0400

    Les PACKET AHAHAHHA

--- src/voicechat/client/Location.py
+++ src/voicechat/client/Location.py
@@ -1,3 +1,5 @@
+import math
+
 class Location:
 
     def __init__(self, x: float = 0, y: float = 0, z: float = 0, world: str = "default"):
@@ -17,3 +19,12 @@ class Location:
 
     def getWorld(self):
         return self.world
+
+    def distance(self, location) -> float:
+        return math.sqrt(self.distanceSquared(location))
+
+    def distanceSquared(self, location) -> float:
+        if not self.world == location.world:
+            return 0.0
+        return ((self.x - location.x) ** 2) + ((self.y - location.y) ** 2) + ((self.z - location.z) ** 2)
+
